6816d770c07d62a85e019b1bc64b884663206880
synerzip <synerzip@mac-pro74.synerzipune.local>
2015-03-02 12:00:49 +0530

    Initial Commit

---
+++ GitRemoteTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitRemoteTest
+//
+//  Created by synerzip on 02/03/15.
+//  Copyright (c) 2015 synerzip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
