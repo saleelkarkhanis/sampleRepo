4d2c02b280c5b41c840f8bb498b0ecec0c1b82bb
synerzip <synerzip@mac-pro74.synerzipune.local>
2015-03-02 12:03:04 +0530

    Comments added just for commiting

--- GitRemoteTest/ViewController.m
+++ GitRemoteTest/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Comment just for commit purpose
 }
 
 - (void)didReceiveMemoryWarning {
